750d4c6e540084fc97b987d9e8e2cc43f6276dbd
carecabrilhante <30707490+carecabrilhante@users.noreply.github.com>
2022-03-04 00:16:56 -0300

    Update contas.py

--- config/lib/contas.py
+++ config/lib/contas.py
@@ -1,28 +1,5 @@
-import yaml
 
-user = []
-pswd = []
+user = ["user1","user2"]
+pswd = ["password1","password2"]
 pro = 0
-def readConfig():
-    with open("./config/config.yaml", 'r', encoding='utf8') as s:
-        stream = s.read()
-    return yaml.safe_load(stream)
 
-try:
-    streamConfig = readConfig()
-    configTimeIntervals = streamConfig['time_intervals']
-    userData = streamConfig['User_login']
-except FileNotFoundError:
-    print('Error: config.yaml file not found, rename EXAMPLE-config.yaml to config.yaml inside /config folder')
-    print('Erro: Arquivo config.yaml não encontrado, renomear EXAMPLE-config.yaml para config.yaml dentro da pasta /config')
-    exit()
-    
-acc = configTimeIntervals['acc']
-n = acc
-def accounts():
-    for w in range(0,n):
-        print("Profile",w)
-        account = str(input("user:\t"))
-        password = str(input("password:\t"))
-        user.append(account)
-        pswd.append(password)
